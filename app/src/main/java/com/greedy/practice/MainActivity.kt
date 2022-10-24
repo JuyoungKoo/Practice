a10ff31ebb2000402dfa1d7d44aae49ddd542f24
juyoungkoo <juyoungkoo9212@gmail.com>
2022-10-24 17:50:53 +0900

    Activity 추가

--- app/src/main/java/com/greedy/practice/MainActivity.kt
+++ app/src/main/java/com/greedy/practice/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.greedy.practice
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.CompoundButton
 import android.widget.SeekBar
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import com.greedy.practice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -57,6 +62,27 @@ class MainActivity : AppCompatActivity() {
 
         })
 
+        val intnet = Intent(this,SubActivity::class.java)
+
+        intent.putExtra("from1", "오늘의 할일!")
+        intent.putExtra("from2",2022)
+
+
+        val resultListener = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            /* result의 resultCode가 RESULT_OK라면 */
+            if(it.resultCode == Activity.RESULT_OK) {
+                /* it.data 자체가 리턴 받은 intent 객체이다.
+                *  returnValue라는 key 값으로 intent에 전달 된 값을 message로 꺼내고 */
+                val message = it.data?.getStringExtra("returnValue")
+                /* MainActivity 위에 짧은 길이로 전달받은 message를 Toast 위젯으로 보여준다. */
+                Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        /* resultListener를 이용한 subactivity 동작 */
+        binding.btnAdd.setOnClickListener {
+            resultListener.launch(intent)
+        }
 
 
 
